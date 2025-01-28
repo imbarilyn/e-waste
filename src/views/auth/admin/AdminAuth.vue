fd4a34f53655897c989b133ed8f03df9b59fb2b5
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-28 17:19:49 +0300

    add AdminAuth page

---
+++ src/views/auth/admin/AdminAuth.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+import NotificationBanner from "@/components/notifications/NotificationBanner.vue";
+</script>
+
+<template>
+
+    <div class="bg-main-300 h-screen">
+      <RouterView #default="{Component, route}">
+        <Transition name="slide-in" mode="out-in" appear>
+          <template v-if="Component">
+            <component :is="Component" :key="route.fullPath" />
+          </template>
+        </Transition>
+
+      </RouterView>
+    </div>
+
+
+
+<!--  <NotificationBanner message="" is-open="" type="" />-->
+
+</template>
+
+<style scoped>
+
+.page-slide-enter-active, .page-slide-enter-active {
+  transition: all 0.35s ease-out;
+}
+.page-slide-enter-from {
+  opacity: 0;
+  transform: translateY(50%);
+}
+.page-slide-leave-to{
+  opacity: 0;
+  transform: translateY(50%);
+}
+
+
+</style>
