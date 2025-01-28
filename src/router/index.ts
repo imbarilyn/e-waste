f3a1b8b98ad093fc8ebf39ddd9f97feda68fb743
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-28 17:12:13 +0300

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,56 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      name: 'Home',
+      path: '/',
+      redirect: '/auth'
+    },
+    {
+      name: 'auth',
+      path: '/admin',
+      component: ()=>import('@/views/auth/admin/AdminAuth.vue'),
+      children: [
+        {
+          name: 'login',
+          path: '',
+            component: ()=>import('@/views/auth/admin/LoginPage.vue'),
+        },
+        {
+          name: 'register',
+          path: 'register',
+            component: ()=>import('@/views/auth/admin/RegisterPage.vue'),
+          meta: {
+            requiresAuth: false,
+          }
+        },
+        {
+          name: 'register',
+          path: 'register',
+          component: ()=>import('@/views/auth/admin/RegisterPage.vue'),
+          meta: {
+            requiresAuth: false,
+          }
+        },
+        {
+          name: 'forgot-password',
+          path: 'forgot-password',
+          component: ()=>import('@/views/auth/admin/ForgotPasswordPage.vue'),
+          meta: {
+            requiresAuth: true,
+          }
+        },
+        {
+          name: 'reset-password',
+          path: 'reset-password',
+          component: ()=>import('@/views/auth/admin/ResetPasswordPage.vue'),
+          meta: {
+            requiresAuth: true,
+          }
+        }
+
+      ]
+    }
 
   ],
 })
