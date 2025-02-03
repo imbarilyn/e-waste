f818f8e339ff7c3a28c109acd4853766a2094df2
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-03 23:06:34 +0300

    add AggregatorPage for auth

---
+++ src/views/auth/aggregator/AggregatorPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <RouterView #default="{Component, route}">
+    <Transition name="page">
+      <template v-if="Component">
+        <component :is="Component" :Key="route.fullPath"/>
+      </template>
+    </Transition>
+
+
+
+  </RouterView>
+
+</template>
+
+<style scoped>
+
+</style>
