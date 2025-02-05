b138bf9147d2f2e3b66a5ecd381cab651d2ab88c
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-05 16:47:10 +0300

    add InventoryPage

---
+++ src/components/aggregator/inventory/InventoryPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import InventoryDataTable from "@/components/aggregator/InventoryDataTable.vue";
+</script>
+
+<template>
+  <div>
+    <div class="py-4">
+      <p class="md:text-2xl text-lg font-semibold">My Inventory</p>
+    </div>
+    <div>
+      <InventoryDataTable/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
