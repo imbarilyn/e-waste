7b07ac3738dad7932d3286ca41167708737e68d5
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-06 23:14:02 +0300

    add Listbox

---
+++ src/components/admin/ListBox.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+interface ListBoxOptions{
+  name: string
+  code?: string
+}
+
+interface ListBoxProps{
+  listProps: ListBoxOptions[]
+  prompt: string
+}
+
+const props = defineProps<ListBoxProps>();
+const selectedOption = ref(props.prompt);
+
+const emits =defineEmits<{
+  (event: 'listChoice', value: string): void
+}>()
+
+watch(()=>selectedOption.value, (value)=>{
+  if(value){
+    emits('listChoice', value)
+  }
+})
+</script>
+
+<template>
+  <div class="">
+    <Listbox v-model="selectedOption">
+      <div class="relative mt-1">
+        <ListboxButton
+          class="relative w-full cursor-default rounded-lg bg-white py-3 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+        >
+          <span class="block truncate">{{ selectedOption }}</span>
+          <span
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <ChevronUpDownIcon
+              class="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
+        </ListboxButton>
+
+        <transition
+          leave-active-class="transition duration-100 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <ListboxOptions
+            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+          >
+            <ListboxOption
+              v-slot="{ active, selected }"
+              v-for="option in props.listProps"
+              :key="option.code"
+              :value="option.name"
+              as="template"
+            >
+              <li
+                :class="[
+                  active ? 'bg-sky-200 text-main-color' : 'text-gray-900',
+                  'relative cursor-default select-none py-2 pl-10 pr-4',
+                ]"
+              >
+                <span
+                  :class="[
+                    selected ? 'font-medium' : 'font-normal',
+                    'block truncate',
+                  ]"
+                >{{ option.name }}</span
+                >
+                <span
+                  v-if="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-main-color"
+                >
+                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
+      </div>
+    </Listbox>
+  </div>
+</template>
+
+
