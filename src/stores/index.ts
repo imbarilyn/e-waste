e95aa3e39267acd66b5026dda1cf785c51689420
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-06 23:22:50 +0300

    add stores

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,2 +1,4 @@
-import * as tabStore  from  '@/stores/tabstore.ts'
-import * as authStore from '@/stores/aggregatorStore.ts'
+import * as aggregatorTabStore  from '@/stores/aggregatorTabstore.ts'
+import * as aggregatorStore from '@/stores/aggregatorStore.ts'
+import  * as adminTabStore from "@/stores/adminTabStore.ts";
+import * as adminStore from "@/stores/adminStore.ts";
