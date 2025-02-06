996a9c55c2a28715467b2112c1fc3f29b0b39701
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-06 23:08:35 +0300

    add adminStore

---
+++ src/stores/adminStore.ts
@@ -0,0 +1,28 @@
+import {defineStore} from 'pinia'
+import {ref} from "vue";
+
+
+export const useAdminStore = defineStore('aggregatorStore', () => {
+    const expandSidebarSmall = ref(false)
+    const collapseSidebar = ref(false)
+
+
+
+
+    const setExpandSidebarSmall = (value: boolean) => {
+        expandSidebarSmall.value = value
+    }
+
+    const setCollapseSidebar = () => {
+        collapseSidebar.value = !collapseSidebar.value
+    }
+
+
+
+    return {
+        expandSidebarSmall,
+        setExpandSidebarSmall,
+        collapseSidebar,
+        setCollapseSidebar
+    }
+})
