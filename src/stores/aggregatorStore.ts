f9cdb6c95eae67128368aea22aca025c365f383d
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-15 10:43:57 +0300

    add Product interface

--- src/stores/aggregatorStore.ts
+++ src/stores/aggregatorStore.ts
@@ -1,6 +1,16 @@
 import {defineStore} from 'pinia'
 import {ref} from "vue";
 
+export interface Product {
+    name: string
+    created_at?: string
+    regularPrice: string
+    stockQuantity: string
+    type: string
+    shortDescription: string
+    weight: string
+    images:File[]
+}
 
 export const useAggregatorStore = defineStore('aggregatorStore', () => {
     const expandSidebarSmall = ref(false)
