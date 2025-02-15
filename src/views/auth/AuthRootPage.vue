3b90621ee559b7500c30868ddfa3a4c7e7c4e56f
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-15 10:45:50 +0300

    add NotificationBanner  component

--- src/views/auth/AuthRootPage.vue
+++ src/views/auth/AuthRootPage.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
+import { useAdminAuthStore } from "@/stores/adminAuthStore";
+import NotificationBanner from "@/components/commonComponent/NotificationBanner.vue";
+import {useAggregatorAuthStore} from "@/stores/aggregatorAuthStore.ts";
 
-import NotificationBanner from "@/components/notifications/NotificationBanner.vue";
+
+const adminAuthStore = useAdminAuthStore()
+const aggregatedAuthStore = useAggregatorAuthStore()
 </script>
 
 <template>
+  <div class="relative">
+    <div class="">
+      <NotificationBanner :message="adminAuthStore.IsAuthenticationError.message || aggregatedAuthStore.IsAuthenticationError.message"
+                          :is-open=adminAuthStore.IsAuthenticationError.isError || aggregatedAuthStore.IsAuthenticationError.isError
+                          :type="adminAuthStore.IsAuthenticationError.type || aggregatedAuthStore.IsAuthenticationError.isError" />
+
+    </div>
 
 
-  <RouterView #default="{Component, route}">
-    <Transition name="page" mode="out-in" appear>
-      <template v-if="Component">
-        <component :is="Component" :key="route.fullPath" />
-      </template>
-    </Transition>
+    <RouterView #default="{Component, route}">
+      <Transition name="page" mode="out-in" appear>
+        <template v-if="Component">
+          <component :is="Component" :key="route.fullPath" />
+        </template>
+      </Transition>
 
-  </RouterView>
+    </RouterView>
+
+  </div>
+
+
+
 
 
 
