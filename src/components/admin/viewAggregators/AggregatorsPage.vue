301032c4c9fc6c8c783643d8b3644858b3250114
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-15 10:43:05 +0300

    removed AggregatorsPage

---
+++ src/components/admin/viewAggregators/AggregatorsPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import AggregatorsDataTable from "@/components/admin/viewAggregators/AggregatorsDataTable.vue";
+</script>
+
+
+  <template>
+    <div>
+      <div class="py-4">
+        <p class="md:text-2xl text-lg font-semibold">My Portfolio</p>
+      </div>
+      <div>
+        <AggregatorsDataTable/>
+      </div>
+    </div>
+  </template>
+
+
+<style scoped>
+
+</style>
