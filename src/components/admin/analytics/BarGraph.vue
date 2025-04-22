38675720e03bce0b70a91ea49fb36d89b0d115ce
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-04-22 15:33:01 +0300

    fetch data for Bar graph

--- src/components/admin/analytics/BarGraph.vue
+++ src/components/admin/analytics/BarGraph.vue
@@ -1,42 +1,51 @@
 <script setup lang="ts">
-import { ref, onMounted, Ref } from 'vue'
+import { ref, onMounted, type Ref } from 'vue'
 import { Chart } from 'chart.js/auto'
+import {useAdminStore} from "@/stores";
 
 // interface Data {
 //   label
 // }
+
+interface Rating {
+  name:string
+  slug: string
+  total: number
+}
+
 const barGraphRef = ref(null)
 const barGraphInstance: Ref<Chart<'bar', number[], string> | null> = ref(null)
+const adminStore = useAdminStore()
 
 const barGraphData = ref({
-  labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+  labels: ['Five star', 'Four star', 'Three start', 'Four two', 'One star'],
   datasets: [
+        // {
+        //   label: 'Phone',
+        //   data: [12, 19, 3, 5],
+        //   backgroundColor: 'rgba(184, 255, 53, 0.6)',
+        //   // borderColor: 'rgba(75, 192, 192, 1)',
+        //   // borderWidth: 1,
+        //   barThickness: 20,
+        //   borderRadius: 6
+        // },
+        // {
+        //   label: 'Computer',
+        //   data: [2, 3, 20, 5],
+        //   backgroundColor: 'rgba(5, 155, 255, 0.6)',
+        //   // borderColor: 'rgba(255, 99, 132, 1)',
+        //   // borderWidth: 1,
+        //   barThickness: 20,
+        //   borderRadius: 6
+        //
+        // },
         {
-          label: 'Phone',
-          data: [12, 19, 3, 5],
-          backgroundColor: 'rgba(184, 255, 53, 0.6)',
-          // borderColor: 'rgba(75, 192, 192, 1)',
-          // borderWidth: 1,
-          barThickness: 20,
-          borderRadius: 6
-        },
-        {
-          label: 'Computer',
-          data: [2, 3, 20, 5],
-          backgroundColor: 'rgba(5, 155, 255, 0.6)',
-          // borderColor: 'rgba(255, 99, 132, 1)',
-          // borderWidth: 1,
-          barThickness: 20,
-          borderRadius: 6
-
-        },
-        {
-          label: 'Other',
-          data: [3, 10, 13, 15],
+          label: 'Rating Report',
+          data: [] as number[],
           backgroundColor: 'rgba(255, 197, 52, 0.6)',
           // borderColor: 'rgba(54, 162, 235, 1)',
           // borderWidth: 1,
-          barThickness: 20,
+          barThickness: 60,
           borderRadius: 6
         }
   ]
@@ -78,19 +87,44 @@ const renderChart = async ()=>{
   }
 }
 
+const isError = ref(false)
 
 onMounted(()=>{
-  renderChart()
+  barGraphData.value.datasets[0].data = []
+  adminStore.getRatings()
+      .then((response)=> {
+        console.log('Rating response', response?.data)
+        if(response?.result === 'success'){
+          response.data.map((rating: Rating)=>{
+            barGraphData.value.datasets[0].data.push(rating.total)
+          })
+          renderChart()
+        }
+        else {
+          isError.value = true
+        }
+      })
+      .catch(error => {
+        console.log('Error rating response', error)
+        isError.value = false
+      })
 })
+
 
 
 </script>
 
 <template>
-  <div>
-    <p class="md:text-2xl text-lg font-semibold">Material type</p>
-    <div class="h-96">
+  <div class="space-y-4">
+    <p class="md:text-2xl text-lg font-semibold">Product Ratings</p>
+    <div class="h-96" v-if="!isError">
       <canvas ref="barGraphRef"></canvas>
+    </div>
+    <div v-else class="pt-4">
+      <div class="flex flex-col justify-center items-center">
+        <img src="/public/images/rating1.png" alt="error" style="width: 250px"/>
+        <span class="text-center">Unable to load rating report data please refresh the page</span>
+      </div>
     </div>
   </div>
 
--- src/components/aggregator/analytics/BarGraph.vue
+++ src/components/aggregator/analytics/BarGraph.vue
@@ -1,44 +1,47 @@
 <script setup lang="ts">
-import { ref, onMounted, Ref } from 'vue'
+import { ref, onMounted} from 'vue'
+import type { Ref } from 'vue'
 import { Chart } from 'chart.js/auto'
+import {useAggregatorStore} from "@/stores";
 
 // interface Data {
 //   label
 // }
 const barGraphRef = ref(null)
 const barGraphInstance: Ref<Chart<'bar', number[], string> | null> = ref(null)
+const aggregatorStore = useAggregatorStore()
 
 const barGraphData = ref({
-  labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+  labels: ['Pending', 'Published', 'Rejected'],
   datasets: [
         {
-          label: 'Phone',
-          data: [12, 19, 3, 5],
-          backgroundColor: 'rgba(184, 255, 53, 0.6)',
-          // borderColor: 'rgba(75, 192, 192, 1)',
-          // borderWidth: 1,
-          barThickness: 20,
-          borderRadius: 6
-        },
-        {
-          label: 'Computer',
-          data: [2, 3, 20, 5],
-          backgroundColor: 'rgba(5, 155, 255, 0.6)',
-          // borderColor: 'rgba(255, 99, 132, 1)',
-          // borderWidth: 1,
-          barThickness: 20,
-          borderRadius: 6
-
-        },
-        {
-          label: 'Other',
-          data: [3, 10, 13, 15],
+          label: 'Product Summary',
+          data: [] as number[],
           backgroundColor: 'rgba(255, 197, 52, 0.6)',
           // borderColor: 'rgba(54, 162, 235, 1)',
           // borderWidth: 1,
-          barThickness: 20,
+          barThickness: 60,
           borderRadius: 6
-        }
+        },
+        // {
+        //   label: 'Computer',
+        //   data: [2, 3, 20, 5],
+        //   backgroundColor: 'rgba(5, 155, 255, 0.6)',
+        //   // borderColor: 'rgba(255, 99, 132, 1)',
+        //   // borderWidth: 1,
+        //   barThickness: 20,
+        //   borderRadius: 6
+        //
+        // },
+        // {
+        //   label: 'Other',
+        //   data: [3, 10, 13, 15],
+        //   backgroundColor: 'rgba(255, 197, 52, 0.6)',
+        //   // borderColor: 'rgba(54, 162, 235, 1)',
+        //   // borderWidth: 1,
+        //   barThickness: 20,
+        //   borderRadius: 6
+        // }
   ]
 })
 
@@ -78,9 +81,28 @@ const renderChart = async ()=>{
   }
 }
 
-
+const isError = ref(false)
 onMounted(()=>{
-  renderChart()
+  barGraphData.value.datasets[0].data = []
+  aggregatorStore.getProductSummary()
+      .then((response)=> {
+        console.log('Rating response', response?.data)
+        if(response?.data){
+          barGraphData.value.datasets[0].data = [
+              response.data.post_counts.pending,
+              response.data.post_counts.publish,
+              response.data.post_counts.reject
+          ]
+          renderChart()
+        }
+        else {
+          isError.value = true
+        }
+      })
+      .catch(error => {
+        console.log('Error rating response', error)
+        isError.value = false
+      })
 })
 
 
@@ -88,7 +110,7 @@ onMounted(()=>{
 
 <template>
   <div>
-    <p class="md:text-2xl text-lg font-semibold">Material type</p>
+    <p class="md:text-2xl text-lg font-semibold">Product Summary</p>
     <div class="h-96">
       <canvas ref="barGraphRef"></canvas>
     </div>
